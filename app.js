3a7f8f2e4a4da53be7e860db3896181c794ad627
sacalamachia <87835657+sacalamachia@users.noreply.github.com>
2021-07-28 13:49:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var helmet = require('helmet');
 
 var app = express();
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://adamdb:Ad840805@cluster0.g7epq.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://adamdb:Ad840805@cluster0.g7epq.mongodb.net/local_library?retryWrites=true'
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
